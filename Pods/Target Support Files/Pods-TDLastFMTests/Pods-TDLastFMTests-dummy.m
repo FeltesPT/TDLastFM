852a21dc47f475456455da5f1af5f8c2369a7271
Tiago Dias <>
2017-07-13 09:13:26 +0100

    Add Alamofire pod

---
+++ Pods/Target Support Files/Pods-TDLastFMTests/Pods-TDLastFMTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TDLastFMTests : NSObject
+@end
+@implementation PodsDummy_Pods_TDLastFMTests
+@end
---
+++ TDLastFM/Services/APIConfig.swift
@@ -0,0 +1,9 @@
+//
+//  APIConfig.swift
+//  TDLastFM
+//
+//  Created by Tiago Dias on 13/07/2017.
+//  Copyright © 2017 Tiago Dias. All rights reserved.
+//
+
+import Foundation
